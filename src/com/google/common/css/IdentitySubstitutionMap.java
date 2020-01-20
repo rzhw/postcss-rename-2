c87709bce724ddc0a977409e04f3e52a7e7f647b
Richard Wang <richardwa@google.com>
2020-01-20 21:15:25 +0900

    Horrifying hax

--- src/com/google/common/css/IdentitySubstitutionMap.java
+++ src/com/google/common/css/IdentitySubstitutionMap.java
@@ -16,7 +16,20 @@
 
 package com.google.common.css;
 
+import com.coveo.nashorn_modules.AbstractFolder;
+import com.coveo.nashorn_modules.Folder;
+import com.coveo.nashorn_modules.Require;
+import com.coveo.nashorn_modules.ResourceFolder;
 import com.google.common.base.Preconditions;
+import jdk.nashorn.api.scripting.NashornScriptEngine;
+
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
 
 /**
  * {@link IdentitySubstitutionMap} is a trivial implementation of
@@ -27,8 +40,117 @@ import com.google.common.base.Preconditions;
  */
 public class IdentitySubstitutionMap implements SubstitutionMap {
 
+  public static class DataFolder extends AbstractFolder {
+    /*ResourceFolder rootDelegate;
+    ResourceFolder nodeModulesDelegate;
+
+    public DataFolder() {
+      rootDelegate = ResourceFolder.create(getClass().getClassLoader(), "com/google/common/css/lol", "UTF-8");
+      nodeModulesDelegate = ResourceFolder.create(getClass().getClassLoader(), "external/npm/node_modules", "UTF-8");
+    }
+
+    public String getFile(String name) {
+      if (name.contains("node_modules")) {
+        throw new RuntimeException("boo " + name);
+      }
+      return rootDelegate.getFile(name);
+    }
+
+    public Folder getFolder(String name) {
+      if (name.endsWith("node_modules")) {
+        InputStream stream = getClass().getClassLoader().getResourceAsStream("external/npm/node_modules/conditional/node_modules/debug/node_modules/ms/node_modules/conditional/lib/util.js");
+        if (stream == null) {
+          return null;
+        }
+
+        //try {
+        //  throw new RuntimeException("hello"+new BufferedReader(new InputStreamReader(stream))
+        //          .lines().collect(Collectors.joining("\n")));
+        //} catch (IOException ex) {
+        //  return null;
+        //}
+        return nodeModulesDelegate.getFolder(name);
+      }
+      return rootDelegate.getFolder(name);
+    }
+
+    public Folder getParent() {
+      return null;
+    }
+
+    public String getPath() {
+      return "/";
+    }*/
+
+
+    private ClassLoader loader;
+    private String resourcePath;
+    private String encoding;
+
+    @Override
+    public String getFile(String name) {
+      String path = resourcePath + "/" + name;
+      System.out.println("getFile " + path);
+      InputStream stream = loader.getResourceAsStream(resourcePath + "/" + name);
+      if (stream == null) {
+        System.out.println("not found!");
+        if (path.contains("tty/index.js")) {
+          System.out.println("tty shim");
+          return "module.exports = {isatty: () => false}";
+        }
+        if (path.contains("util/index.js")) {
+          System.out.println("util shim");
+          return "module.exports = {format: () => '', inspect: () => ''}";
+        }
+        return null;
+      }
+
+      return new BufferedReader(new InputStreamReader(stream)).lines().collect(Collectors.joining("\n"));
+    }
+
+    @Override
+    public Folder getFolder(String name) {
+      System.out.println("getFolder " + (resourcePath + "/" + name));
+      if (name.contains("node_modules") && getParent() == null) {
+        return new DataFolder(
+                loader, "external/npm/node_modules/conditional/node_modules/debug/node_modules/ms/node_modules", null, getPath() + name + "/", encoding);
+      }
+      return new DataFolder(
+              loader, resourcePath + "/" + name, this, getPath() + name + "/", encoding);
+    }
+
+    private DataFolder(
+            ClassLoader loader, String resourcePath, Folder parent, String displayPath, String encoding) {
+      super(parent, displayPath);
+      this.loader = loader;
+      this.resourcePath = resourcePath;
+      this.encoding = encoding;
+    }
+  }
+
+  public static DataFolder create(ClassLoader loader, String path, String encoding) {
+    return new DataFolder(loader, path, null, "/", encoding);
+  }
+
+  NashornScriptEngine engine;
+
+  public IdentitySubstitutionMap() {
+    System.setProperty("nashorn.args", "--language=es6");
+    engine = (NashornScriptEngine) new ScriptEngineManager().getEngineByName("nashorn");
+    try {
+      Require.enable(engine, create(getClass().getClassLoader(), "com/google/common/css/lol", "UTF-8"));
+    } catch (ScriptException e) {
+      throw new RuntimeException("Couldn't initialize nashorn-commonjs-modules", e);
+    }
+  }
+
   @Override
   public String get(String key) {
+    try {
+      engine.eval("require('./identity-substitution-map')().get(\"" + key + "\")");
+    } catch (ScriptException e) {
+      throw new RuntimeException("Eval failed", e);
+    }
     Preconditions.checkNotNull(key);
     return key;
   }
