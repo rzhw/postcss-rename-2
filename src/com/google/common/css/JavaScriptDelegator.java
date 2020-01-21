f82cd9c09c2ea56caca722c607d5218e4a2bf2e6
Richard Wang <richardwa@google.com>
2020-01-21 18:12:18 +0900

    Prefer individual Java imports

--- src/com/google/common/css/JavaScriptDelegator.java
+++ src/com/google/common/css/JavaScriptDelegator.java
@@ -11,7 +11,10 @@ import javax.script.ScriptException;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class JavaScriptDelegator {
